12d16824a360eed2180bd79f563d7950b1504543
r.m.popov <r.m.popov@krasnodar.pro>
2023-09-14 16:41:49 +0300

    build: readme change

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main class="about">
+    <FaContainer>
+      <h1>About</h1>
+      <div>
+        <p>This is Roman Popov pet project.</p>
+        <p>You can contact me with:</p>
+        <ul>
+          <li>Email: <FaLink to="mailto:romanpopov.webdev@gmail.com">romanpopov.webdev@gmail.com</FaLink></li>
+          <li>Telegram: <FaLink to="https://t.me/fingersdaggers">@fingersdaggers</FaLink></li>
+        </ul>
+      </div>
+    </FaContainer>
+  </main>
 </template>
 
+<script setup lang="ts">
+import FaContainer from '@/components/ui/FaContainer.vue'
+import FaLink from '@/components/ui/FaLink.vue'
+</script>
 <style>
-@media (width >= 1024px) {
-  .about {
-    display: flex;
-    align-items: center;
-    min-height: 100vh;
-  }
+.about {
+  padding: 100px 0;
 }
 </style>
